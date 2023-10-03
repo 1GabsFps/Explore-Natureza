bbe94f7053fc1becae26e6b6606185f6b7f6db0a
1GabsFps <gabriel.neco@live.com>
2023-10-03 09:46:36 -0300

    responsividade e js

--- js/faq.js
+++ js/faq.js
@@ -1,21 +1,11 @@
-const imagens = document.querySelectorAll('.img-scroll img');
-const textos = document.querySelectorAll('.natureza-text');
 
-imagens.forEach((imagem, index) => {
-    imagem.addEventListener('click', () => {
-      // Oculta todos os textos
-    textos.forEach(texto => {
-        texto.style.display = 'none';
-    });
-
-
-    textos[index].style.display = 'block';
-    });
-});
-    const faqs = document.querySelectorAll(".faq");
+const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active");
     });
 });
+
+
+
